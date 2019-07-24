4701af07fa9cbc58984ff491fdc96b8473b5e43c
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-24 17:16:10 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0 
  new_array = []
  while i < collection.length do
-   new_array << yield(collection[i]) if yield(collection[i]) == true 
+   new_array << collection[i] if yield(collection[i]) == true 
    i +=1
  end
  new_array
